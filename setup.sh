dfbcc29dfb6c54541bea11354528896c544b7e3b
Yi-Kuang Chen <twolights@gmail.com>
2018-10-27 02:30:12 +0800

    Added Droid Sans Mono font file

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 SOURCE_PATH=~/test.src
 INSTALL_PATH=$SOURCE_PATH/macos-provisioning
+TEMP_DROID_SANS_PATH=/tmp/droid-sans-mono.zip
 
 sudo xcodebuild -license
 xcode-select --install
@@ -22,8 +23,16 @@ sudo chsh -s /bin/zsh $USER
 
 bash <(curl -L https://raw.github.com/twolights/meowrc/master/setup.sh)
 
+pushd /tmp
+wget -O $TEMP_DROID_SANS_PATH https://www.fontsquirrel.com/fonts/download/droid-sans-mono
+unzip $TEMP_DROID_SANS_PATH
+mv /tmp/DroidSansMono.ttf ~/Library/Fonts
+rm -f $TEMP_DROID_SANS_PATH
+popd
+
+cp files/com.apple.Terminal.plist ~/Library/Preferences/
+
 # TODO
-# Terminal settings
-# Scroll direction
-# Install Droid Sans
 # Install VPN
+# Dock mode
+# Swipe fingers
